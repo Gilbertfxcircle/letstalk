9e29e61e8476b6e518853d927bfa14f599261db2
Gilbertfxcircle <tumusiimehuzaifah@gmail.com>
2025-07-11 14:03:38 +0300

    Add files via upload
    new update

--- server.js
+++ server.js
@@ -2,22 +2,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+const PORT = process.env.PORT || 10000;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 }).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT || 10000, () => {
-    console.log(`Server running on port ${process.env.PORT || 10000}`);
+  console.log("MongoDB connected");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
+}).catch(err => {
+  console.error("MongoDB connection error:", err);
 });
