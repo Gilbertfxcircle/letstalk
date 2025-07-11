7cb3afde9207091e4c932520cb7450605c1a19f9
Gilbertfxcircle <tumusiimehuzaifah@gmail.com>
2025-07-11 17:26:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
+// Routes
 const otpRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+
 app.use('/api/otp', otpRoutes);
+app.use('/api/posts', postRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
